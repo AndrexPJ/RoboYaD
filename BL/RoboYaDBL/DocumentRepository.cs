428a81f0815b8242c90d9f12bd63270519743d66
ilya <ilia12r@gmail.com>
2015-05-30 15:27:30 +0500

    added bl

---
+++ BL/RoboYaDBL/DocumentRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Linq.Expressions;
+
+namespace RoboYaDBL
+{
+
+    class HistoryDocument
+    {
+
+    }
+
+    class CommandDocument
+    {
+
+    }
+
+    class DocumentRepository<TDocument> : IDocumentRepository<TDocument>
+    {
+        private IMongoCollection<TDocument> collection;
+        public DocumentRepository(IMongoDatabase db, string collectionName)
+        {
+            this.collection = db.GetCollection<TDocument>(collectionName);
+        }
+        public TDocument ReadSync(Expression<Func<TDocument, bool>> filter)
+        {
+            var task = this.collection.FindOneAndDeleteAsync<TDocument>(new ExpressionFilterDefinition<TDocument>(filter));
+            return task.Result;
+        }
+        public void WriteSync(TDocument document)
+        {
+            var task = this.collection.InsertOneAsync(document);
+            task.Wait();
+        }
+    }
+
+}
---
+++ BL/RoboYaDBL/IDocumentRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Linq.Expressions;
+namespace RoboYaDBL
+{
+    interface IDocumentRepository<TDocument>
+    {
+        TDocument ReadSync(Expression<Func<TDocument, bool>> filter);
+        void WriteSync(TDocument document);
+    }
+}
