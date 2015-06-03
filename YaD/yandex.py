a8d3707fee713152bdc300faf3aa59fba7a8d364
AndreyGithub2015 <hihi@trickmail.net>
2015-06-03 05:52:47 +0500

    method GetBannerPhrases
    db.prices.insert{"Min, "Max", "PremiumMin", "PremiumMax", "Phrase",
    "datetime"}

--- YaD/yandex.py
+++ YaD/yandex.py
@@ -1,5 +1,10 @@
-﻿import json, urllib.request
+﻿import json, urllib.request, time
 from pymongo import MongoClient
+from datetime import datetime
+
+client = MongoClient('mongodb://ilya:ilya@ds049641.mongolab.com:49641/yandex_bot')
+
+db = client.yandex_bot
 
 #адрес для отправки json-запросов
 url = 'https://api-sandbox.direct.yandex.ru/v4/json/'
@@ -12,45 +17,45 @@ login = 'directrf@yandex.ru'
 
 #структура входных данных (словарь)
 data = {
-  "method": "GetClientsList",
+  "method": "GetBannerPhrases",
   "token": "a39e6a0f43534d06a9ca3ebc997db087",
   "locale": "ru",
-  "param": {
-  }
+  "param": [
+		1008398, 
+		1008399,
+		1008400
+	]
 }
-
 #конвертировать словарь в JSON-формат и перекодировать в UTF-8
 jdata = json.dumps(data, ensure_ascii=False).encode('utf8')
 
-#выполнить запрос
-response = urllib.request.urlopen(url, jdata)
+while 1:
+	#выполнить запрос
+	response = urllib.request.urlopen(url, jdata)
 
-#результаты запроса в JSON-формате
-result = response.read().decode('utf-8')
+	#результаты запроса в JSON-формате
+	result = response.read().decode('utf-8')
 
-#преобразование из JSON-формата в словарь
-unJsonYandexResult = json.loads(result);
+	#преобразование из JSON-формата в словарь
+	unJsonYandexResult = json.loads(result);
 
-print(unJsonYandexResult)
+	#print(unJsonYandexResult)
 
-client = MongoClient('mongodb://ilya:ilya@ds049641.mongolab.com:49641/yandex_bot')
+	#print(collection)
 
-db = client.yandex_bot
+	#db.bar.insert({"loc": "789"})
 
-collection = db.AspNetUsers
+	#for c in db.bar.find():
+	#	print(db.bar.find_one({}))
 
-#print(collection)
-
-#db.bar.insert({"loc": "789"})
-
-print("-----------------------------------------\n")
-#for c in db.bar.find():
-#	print(db.bar.find_one({}))
-
-#for (k,v) in unJsonYandexResult:
-#	print(c["data"][0])
-for key,value in unJsonYandexResult.items():
-	for value2 in value:
-		for key3,value3 in value2.items():
-			print(key3, "->", value3)
-		print("\n   +++++++++++++++++++++++++++++++   \n\n")
+	#for (k,v) in unJsonYandexResult:
+	#	print(c["data"][0])
+	for key,value in unJsonYandexResult.items():
+		for value2 in value:
+			db.prices.insert({"Min": value2["Min"], "Max": value2["Max"],
+			"PremiumMin": value2["PremiumMin"], "PremiumMax": value2["PremiumMax"],
+			"Phrase": value2["Phrase"], 
+			"datetime": datetime.strftime(datetime.now(), "%Y.%m.%d %H:%M:%S")})
+			
+	print(datetime.strftime(datetime.now(), "%Y.%m.%d %H:%M:%S"))
+	time.sleep(10)
